deaeae90a8c9dbe9ac80a3f1be421928488660f8
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-03-04 02:07:30 +0300

    Asp.Net Core Mvc Projesi oluşturma-3: IRepository generic interface’sinin oluşturulması

---
+++ UdemyRealWordUnitTest.Web/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyRealWordUnitTest.Web.Repository
+{
+    internal interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+
+        Task<TEntity> GetById(int id);
+
+        Task Create(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
