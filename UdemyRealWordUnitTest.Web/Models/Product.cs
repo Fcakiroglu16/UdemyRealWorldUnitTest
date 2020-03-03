7f82f0c7f3755792c40321c656bb455179ac73f0
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-03-04 01:54:33 +0300

    Add project files.

---
+++ UdemyRealWordUnitTest.Web/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UdemyRealWordUnitTest.Web.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public int? Stock { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ UdemyRealWordUnitTest.Web/Models/UdemyUnitTestDBContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace UdemyRealWordUnitTest.Web.Models
+{
+    public partial class UdemyUnitTestDBContext : DbContext
+    {
+        public UdemyUnitTestDBContext()
+        {
+        }
+
+        public UdemyUnitTestDBContext(DbContextOptions<UdemyUnitTestDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Color).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(200);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
