93d81a19e79de766623676a43f939f06c2251a2e
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-03-04 18:20:50 +0300

    Delete methodunun test edilmesi-5

--- UdemyRealWordUnitTest.Test/ProductControllerTest.cs
+++ UdemyRealWordUnitTest.Test/ProductControllerTest.cs
@@ -278,5 +278,18 @@ namespace UdemyRealWordUnitTest.Test
 
             Assert.IsType<RedirectToActionResult>(result);
         }
+
+        [Theory]
+        [InlineData(1)]
+        public async void DeleteConfirmed_ActionExecutes_DeleteMethodExecute(int productId)
+        {
+            var product = products.First(x => x.Id == productId);
+
+            _mockRepo.Setup(repo => repo.Delete(product));
+
+            await _controller.DeleteConfirmed(productId);
+
+            _mockRepo.Verify(repo => repo.Delete(It.IsAny<Product>()), Times.Once);
+        }
     }
 }
