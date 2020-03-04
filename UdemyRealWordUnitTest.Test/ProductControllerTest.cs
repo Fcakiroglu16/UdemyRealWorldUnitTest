2c857d20c01fd2735a5f3b98c1ea7f2c2d8b6baa
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-03-04 14:09:41 +0300

    Index methodunun test edilmesi-1/2

---
+++ UdemyRealWordUnitTest.Test/ProductControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UdemyRealWordUnitTest.Web.Controllers;
+using UdemyRealWordUnitTest.Web.Models;
+using UdemyRealWordUnitTest.Web.Repository;
+using Xunit;
+
+namespace UdemyRealWordUnitTest.Test
+{
+    public class ProductControllerTest
+    {
+        private readonly Mock<IRepository<Product>> _mockRepo;
+
+        private readonly ProductsController _controller;
+
+        private List<Product> products;
+
+        public ProductControllerTest()
+        {
+            _mockRepo = new Mock<IRepository<Product>>();
+            _controller = new ProductsController(_mockRepo.Object);
+
+            products = new List<Product>() { new Product { Id = 1, Name = "Kalem", Price = 100, Stock = 50, Color = "Kırmızı" },
+            new Product { Id = 2, Name = "Defter", Price = 200, Stock = 500, Color = "Mavi" }};
+        }
+
+        [Fact]
+        public async void Index_ActionExecutes_ReturnView()
+        {
+            var result = await _controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public async void Index_ActionExecutes_ReturnProductList()
+        {
+            _mockRepo.Setup(repo => repo.GetAll()).ReturnsAsync(products);
+
+            var result = await _controller.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var productList = Assert.IsAssignableFrom<IEnumerable<Product>>(viewResult.Model);
+
+            Assert.Equal<int>(2, productList.Count());
+        }
+    }
+}
