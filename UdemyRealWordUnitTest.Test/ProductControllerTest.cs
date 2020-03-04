9ab1fd3634550fb703171ba613fab3c3f5c953a3
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-03-04 14:44:46 +0300

    Detail methodunun test edilmesi-3

--- UdemyRealWordUnitTest.Test/ProductControllerTest.cs
+++ UdemyRealWordUnitTest.Test/ProductControllerTest.cs
@@ -72,5 +72,23 @@ namespace UdemyRealWordUnitTest.Test
 
             Assert.Equal<int>(404, redirect.StatusCode);
         }
+
+        [Theory]
+        [InlineData(1)]
+        public async void Details_ValidId_ReturnProduct(int productId)
+        {
+            Product product = products.First(x => x.Id == productId);
+
+            _mockRepo.Setup(repo => repo.GetById(productId)).ReturnsAsync(product);
+
+            var result = await _controller.Details(productId);
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var resultProduct = Assert.IsAssignableFrom<Product>(viewResult.Model);
+
+            Assert.Equal(product.Id, resultProduct.Id);
+            Assert.Equal(product.Name, resultProduct.Name);
+        }
     }
 }
