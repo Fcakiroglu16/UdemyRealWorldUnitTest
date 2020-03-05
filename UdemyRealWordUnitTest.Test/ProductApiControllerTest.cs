4e67593975593468615304acfe71792ad317b27d
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-03-05 15:32:08 +0300

    GetProduct() methodunun test edilmesi

---
+++ UdemyRealWordUnitTest.Test/ProductApiControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UdemyRealWordUnitTest.Web.Controllers;
+using UdemyRealWordUnitTest.Web.Models;
+using UdemyRealWordUnitTest.Web.Repository;
+using Xunit;
+
+namespace UdemyRealWordUnitTest.Test
+{
+    public class ProductApiControllerTest
+    {
+        private readonly Mock<IRepository<Product>> _mockRepo;
+        private readonly ProductsApiController _controller;
+
+        private List<Product> products;
+
+        public ProductApiControllerTest()
+        {
+            _mockRepo = new Mock<IRepository<Product>>();
+            _controller = new ProductsApiController(_mockRepo.Object);
+
+            products = new List<Product>() { new Product { Id = 1, Name = "Kalem", Price = 100, Stock = 50, Color = "Kırmızı" },
+            new Product { Id = 2, Name = "Defter", Price = 200, Stock = 500, Color = "Mavi" }};
+        }
+
+        [Fact]
+        public async void GetProduct_ActionExecutes_ReturnOkResultWithProduct()
+        {
+            _mockRepo.Setup(x => x.GetAll()).ReturnsAsync(products);
+
+            var result = await _controller.GetProduct();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            var returnProducts = Assert.IsAssignableFrom<IEnumerable<Product>>(okResult.Value);
+
+            Assert.Equal<int>(2, returnProducts.ToList().Count);
+        }
+    }
+}
---
+++ UdemyRealWordUnitTest.Web/Controllers/ProductsApiController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UdemyRealWordUnitTest.Web.Models;
+using UdemyRealWordUnitTest.Web.Repository;
+
+namespace UdemyRealWordUnitTest.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase
+    {
+        private readonly IRepository<Product> _repository;
+
+        public ProductsApiController(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/ProductsApi
+        [HttpGet]
+        public async Task<IActionResult> GetProduct()
+        {
+            var products = await _repository.GetAll();
+
+            return Ok(products);
+        }
+
+        // GET: api/ProductsApi/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _repository.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // PUT: api/ProductsApi/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public IActionResult PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _repository.Update(product);
+
+            return NoContent();
+        }
+
+        // POST: api/ProductsApi
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<IActionResult> PostProduct(Product product)
+        {
+            await _repository.Create(product);
+
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/ProductsApi/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _repository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Delete(product);
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            Product product = _repository.GetById(id).Result;
+
+            if (product == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
