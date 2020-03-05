5406f9afefe904ceedfd63a0c7b14e2b4c9d5c84
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-03-05 15:41:11 +0300

    GetProduct(int) methodunun test edilmesi

--- UdemyRealWordUnitTest.Test/ProductApiControllerTest.cs
+++ UdemyRealWordUnitTest.Test/ProductApiControllerTest.cs
@@ -40,5 +40,18 @@ namespace UdemyRealWordUnitTest.Test
 
             Assert.Equal<int>(2, returnProducts.ToList().Count);
         }
+
+        [Theory]
+        [InlineData(0)]
+        public async void GetProduct_IdInValid_ReturnNotFound(int productId)
+        {
+            Product product = null;
+
+            _mockRepo.Setup(x => x.GetById(productId)).ReturnsAsync(product);
+
+            var result = await _controller.GetProduct(productId);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
     }
 }
